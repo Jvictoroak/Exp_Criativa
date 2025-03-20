113a8966818ed73ed3a007a9496f2d8ecf4f0252
João Victor Carvalho <joaovictor.crvl@gmail.com>
2025-03-20 18:55:59 -0300

    tagbar como component

--- theme/component/tagbar/component.js
+++ theme/component/tagbar/component.js
@@ -1,15 +1,15 @@
 document.addEventListener("DOMContentLoaded", function () {
     // Pega o conteúdo do header.html
-    fetch("../../theme/component/navbar/component.html")
+    fetch("../../theme/component/tagbar/component.html")
         .then(response => response.text())
         .then(data => {
-            document.getElementById("navbar").innerHTML = data;
+            document.getElementById("tagbar").innerHTML = data;
 
             // Cria um elemento <link> para importar o CSS
             const link = document.createElement("link");
             link.rel = "stylesheet";
-            link.href = "../../theme/component/navbar/component.css"; // Caminho do CSS
+            link.href = "../../theme/component/tagbar/component.css"; // Caminho do CSS
             document.head.appendChild(link);
         })
-        .catch(error => console.error("Erro ao carregar o navbar:", error));
+        .catch(error => console.error("Erro ao carregar o tagbar:", error));
 });
