a641d82a3dc6336ecdd8a483774c7b39f1a0ff7b
João Victor Carvalho <joaovictor.crvl@gmail.com>
2025-03-24 18:28:51 -0300

    asdasdas

---
+++ theme/assets/pages/js/index.js
@@ -0,0 +1,10 @@
+const vermais = document.getElementById('vermais');
+const popup_publicacao = document.getElementById('popup-publicacao'); // Substitua pelo ID do elemento desejado
+
+vermais.addEventListener('click', () => {
+    popup_publicacao.classList.toggle('block'); // Substitua 'nova-classe' pelo nome da classe desejada
+});
+
+popup_publicacao.addEventListener('click', () => {
+    popup_publicacao.classList.toggle('block'); // Substitua 'nova-classe' pelo nome da classe desejada
+})
