98f5f7dcc5293a4a5a37fcdd197e0656cd4fdac8
João Victor Carvalho <joaovictor.crvl@gmail.com>
2025-03-25 18:23:17 -0300

    poup de comentários no perfil

--- theme/assets/pages/js/perfil.js
+++ theme/assets/pages/js/perfil.js
@@ -107,4 +107,19 @@ document.addEventListener("DOMContentLoaded", () => {
         salvarBtn.disabled = true;
         successAlert();
     });
+
+    const vermais = document.getElementsByClassName('vermais');
+    const popup = document.getElementById('popup-publicacao');
+    const popup_fechar = document.getElementById('fechar-popup');
+
+    Array.from(vermais).forEach(element => {
+        element.addEventListener('click', () => {
+            popup.classList.toggle('block'); 
+        });
+    });
+
+    popup_fechar.addEventListener('click', () => {
+        popup.classList.toggle('block'); 
+    })
+
 });
