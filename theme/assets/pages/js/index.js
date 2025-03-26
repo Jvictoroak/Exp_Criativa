beabcbf0da68668d43ddbed8b167113f136c9004
João Victor Carvalho <joaovictor.crvl@gmail.com>
2025-03-26 20:42:41 -0300

    até aq tá funcionando

--- theme/assets/pages/js/index.js
+++ theme/assets/pages/js/index.js
@@ -1,11 +1,25 @@
-const vermais = document.getElementById('vermais');
-const popup_publicacao = document.getElementById('popup-publicacao');
+// const vermais = document.getElementById('vermais');
+// const popup_publicacao = document.getElementById('popup-publicacao');
+// const popup_fechar = document.getElementById('fechar-popup');
+
+// vermais.addEventListener('click', () => {
+//     popup_publicacao.classList.toggle('block');
+// });
+
+// popup_fechar.addEventListener('click', () => {
+//     popup_publicacao.classList.toggle('block'); 
+// })
+
+const vermais = document.getElementsByClassName('vermais');
+const popup = document.getElementById('popup-publicacao');
 const popup_fechar = document.getElementById('fechar-popup');
 
-vermais.addEventListener('click', () => {
-    popup_publicacao.classList.toggle('block');
+Array.from(vermais).forEach(element => {
+    element.addEventListener('click', () => {
+        popup.classList.toggle('block');
+    });
 });
 
 popup_fechar.addEventListener('click', () => {
-    popup_publicacao.classList.toggle('block'); 
-})
+    popup.classList.toggle('block');
+});
