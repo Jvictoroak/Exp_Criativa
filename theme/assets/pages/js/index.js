b76d7d7c6101bde4011211dbfdbe4eed34fbbbdf
João Victor Carvalho <joaovictor.crvl@gmail.com>
2025-03-26 21:56:08 -0300

    ASDAS

--- theme/assets/pages/js/index.js
+++ theme/assets/pages/js/index.js
@@ -1,15 +1,3 @@
-// const vermais = document.getElementById('vermais');
-// const popup_publicacao = document.getElementById('popup-publicacao');
-// const popup_fechar = document.getElementById('fechar-popup');
-
-// vermais.addEventListener('click', () => {
-//     popup_publicacao.classList.toggle('block');
-// });
-
-// popup_fechar.addEventListener('click', () => {
-//     popup_publicacao.classList.toggle('block'); 
-// })
-
 const vermais = document.getElementsByClassName('vermais');
 const popup = document.getElementById('popup-publicacao');
 const popup_fechar = document.getElementById('fechar-popup');
