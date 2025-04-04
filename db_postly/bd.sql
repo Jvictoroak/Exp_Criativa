287746a97d358979ea367a9b8f282b3a0eba8f51
ArthurDP-03 <arthuroliveira.c50@gmail.com>
2025-04-04 17:59:20 -0300

    Editando banco de dados!

--- db_postly/bd.sql
+++ db_postly/bd.sql
@@ -5,7 +5,7 @@ USE postly_db;
 /* Lógico_1: */
 
 CREATE TABLE usuario (
-    id int PRIMARY KEY,
+    id int PRIMARY KEY AUTO_INCREMENT,
     senha varchar(50),
     data date,
     telefone varchar(20),
@@ -15,7 +15,7 @@ CREATE TABLE usuario (
 );
 
 CREATE TABLE publicacao (
-    id int PRIMARY KEY,
+    id int PRIMARY KEY AUTO_INCREMENT,
     titulo varchar(50),
     foto blob,
     descricao varchar(300),
@@ -23,26 +23,26 @@ CREATE TABLE publicacao (
 );
 
 CREATE TABLE tags (
-    nome varchar(50),
-    id int PRIMARY KEY
+    id int PRIMARY KEY AUTO_INCREMENT,
+    nome varchar(50)
 );
 
 CREATE TABLE tem (
+    id int PRIMARY KEY AUTO_INCREMENT,
     fk_publicacao_id int,
-    fk_tags_id int,
-    id int PRIMARY KEY
+    fk_tags_id int
 );
- 
+
 ALTER TABLE publicacao ADD CONSTRAINT FK_publicacao_2
     FOREIGN KEY (fk_usuario_id)
     REFERENCES usuario (id)
     ON DELETE CASCADE;
- 
+
 ALTER TABLE tem ADD CONSTRAINT FK_tem_2
     FOREIGN KEY (fk_publicacao_id)
     REFERENCES publicacao (id)
     ON DELETE SET NULL;
- 
+
 ALTER TABLE tem ADD CONSTRAINT FK_tem_3
     FOREIGN KEY (fk_tags_id)
     REFERENCES tags (id)
