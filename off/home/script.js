f97dee0a094568afa6ea2ac55becdfad5bce20e4
João Victor Carvalho <joaovictor.crvl@gmail.com>
2025-04-11 09:31:57 -0300

    ajustando as pastas

--- off/home/script.js
+++
@@ -1,6 +0,0 @@
-// Add basic interactivity (e.g., alert on button click)
-document.querySelectorAll('.post-actions button').forEach(button => {
-    button.addEventListener('click', () => {
-        alert(`You clicked ${button.textContent}!`);
-    });
-});
