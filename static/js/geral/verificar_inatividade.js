41f1a3771672fa5382affbfa3e29743d71accd75
Zhou <141890618+RobertoZhou@users.noreply.github.com>
2025-05-21 19:57:56 -0300

    Add SwatAlert

--- static/js/geral/verificar_inatividade.js
+++ static/js/geral/verificar_inatividade.js
@@ -1,16 +1,31 @@
 document.addEventListener('DOMContentLoaded', () => {
     // Defina o tempo limite de inatividade em milissegundos
-    let inactivityTime = 20 * 60 * 1000; // 12 segundos, por exemplo
+    let inactivityTime = 0.2 * 60 * 1000; // 12 segundos, por exemplo
     let timer;
 
     function resetTimer() {
         console.log('Atividade detectada, resetando o timer...');
         clearTimeout(timer);
-        timer = setTimeout(logoutUser, inactivityTime);
+        timer = setTimeout(showInactivityAlert, inactivityTime);
+    }
+
+    function showInactivityAlert() {
+        console.log('Usuário inativo, exibindo alerta...');
+
+        // Exibe o alerta usando SweetAlert
+        Swal.fire({
+            title: 'Sessão expirada',
+            text: 'Você ficou inativo por um tempo e será redirecionado para a página de login.',
+            icon: 'warning',
+            confirmButtonText: 'OK',
+            allowOutsideClick: false
+        }).then(() => {
+            logoutUser();
+        });
     }
 
     function logoutUser() {
-        console.log('Usuário inativo por 2 minutos, encerrando sessão...');
+        console.log('Usuário inativo, encerrando sessão...');
         fetch('/logout', { credentials: 'include' })
             .then(() => {
                 window.location.href = '/login';
